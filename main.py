2d118aaef1bdc6e73e7bc081e8879859a2bfcdde
AaronLee <li8366388@gmail.com>
2025-03-26 20:12:25 +0800

    main.py

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import requests
 from lxml import etree
-# 发给谁
+# 发送
 url = "https://www.hetushu.com/book/5626/4196405.html"
 
-# 伪装自己
+# 伪装
 headers = {
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36'
 }
 
-# 发送请求
+# 请求
 resp = requests.get(url,headers = headers)
 resp.encoding="UTF-8"  # 转UTF-8
-# 响应信息
+# 响应
 # print(resp.text)
 
-e = etree.HTML(resp.text)
+e = etree.HTML(resp.text) # 解析网页
 title = e.xpath('//h2/text()')[0]   # ···/text() 取文本内容
 paragraphs = '\n'.join(e.xpath('//div[@id="content"]/div/text()')) # ···/text() 取文本内容
 
@@ -22,7 +22,7 @@ paragraphs = '\n'.join(e.xpath('//div[@id="content"]/div/text()')) # ···/text
 
 # print(info)
 
-# 保存文件
+# 保存
 
 with open('D:/AnYujian/Desktop/斗罗大陆IV终极斗罗.txt','w',encoding='UTF-8') as f:
     f.write(title + '\n\n' + p + '\n\n')
